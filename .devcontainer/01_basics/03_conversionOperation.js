3101e776b30967d9f654c3c56af7b19fbf61d9b8
Prasanna Dukhi <140623262+pdukhi01@users.noreply.github.com>
2023-12-08 05:36:03 +0000

    conversionOperation

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score=true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log( typeof valueInNumber);
+console.log(valueInNumber);
+
+let someNumber=33
+let stringNumber=string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
